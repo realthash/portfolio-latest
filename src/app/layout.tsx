588d2a28463869d3bcc3466f70cfda24a87da279
Thashmika Rathnayake <thashmikarathnayake11@gmail.com>
2026-03-18 01:42:18 +0530

    Title rechanage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "Thash's Portfolio ",
+  title: "Build With THASH",
   description: "Sofware Engineer Portfolio showcasing hand on projects, achievements and skills.",
 };
 
